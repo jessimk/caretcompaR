f61d412c91639651674af95c83fc1a93543197e9
Talha Siddiqui <talhaadnan100@gmail.com>
2019-02-08 18:39:03 -0800

    Create basic function script structure

---
+++ R/split.R
@@ -0,0 +1,20 @@
+#' Title
+#'
+#' @param X 
+#' @param y 
+#' @param ptrain 
+#' @param ptest
+#' @param pvalidation
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#' 
+split <- function(X ,y ,ptrain, ptest, pvalidation){
+  
+  
+  
+  return(X_train, y_train, X_validation, y_validation, X_train_validation, y_train_validation, X_test, y_test)
+}
