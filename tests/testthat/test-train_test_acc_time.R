14657b1ef13993ce6fe0026eb91df753d708b3a9
Birinder <samdsilva1469@gmail.com>
2019-02-09 12:46:10 -0800

    Added tests for test train accuracy

--- tests/testthat/test-train_test_acc_time.R
+++ tests/testthat/test-train_test_acc_time.R
@@ -3,3 +3,33 @@ context("test-train_test_acc_time")
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+# SAMPLE DATA
+
+models <- list('lm','rf','knn','svmLinearWeights')
+X_train <- data.frame(col1=sample(1:100,100, replace = TRUE ), 
+                      col2= sample(50:150,100, replace = TRUE ),
+                      col2= sample(500:750,100, replace = TRUE ))
+X_test <- data.frame(col1=sample(1:100,50, replace = TRUE ), col2= sample(50:150,50, replace = TRUE ))
+y_train <- runif(4,50,100)
+y_test <- runif(5,10,50)
+result <- data.frame(model = c('lm','rf','knn','svmLinearWeights'), 
+                      train_accuracy = runif(4,50,100),
+                      test_accuracy = runif(4,50,100),
+                      variance = runif(4,0,50),
+                      fit_time = runif(4,1,6),
+                      predict_time = runif(4,1,6),
+                      total_time = runif(4,2,12)
+)
+
+# Sufficient inputs tests
+
+test_that("insufficient input", {
+  expect_error(train_test_acc_time(models, X_train, y_train, X_test, y_test),
+               "models list input is missing")
+  expect_error(train_test_acc_time(models, X_train, y_train, X_test),
+               "y-test dataframe is missing")
+})
+
+
+
